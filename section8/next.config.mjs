7d3ae4c5828288fa8092def54936b326ed67e82a
jio <pennicillium@daum.net>
2024-11-01 20:52:28 +0900

    [feat] Image 컴포넌트 사용한 최적화

--- section8/next.config.mjs
+++ section8/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//이미지호스팅 사용시 remotePatterns에 hostname추가
+const nextConfig = {
+  images: { remotePatterns: [{ hostname: "res.cloudinary.com" }] },
+};
 
 export default nextConfig;
