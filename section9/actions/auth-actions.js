03019caf36952bcd2e59847ddb92a26532d05e1b
jio <pennicillium@daum.net>
2024-11-03 14:52:37 +0900

    [feat] password hash화 저장

--- section9/actions/auth-actions.js
+++ section9/actions/auth-actions.js
@@ -1,4 +1,9 @@
-"use server"; // 서버 액션 역할 하는 기능 생성을 위한 지시문
+"use server";
+import { createAuthSession } from "@/lib/auth";
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+// 서버 액션 역할 하는 기능 생성을 위한 지시문
 export async function signup(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -15,4 +20,22 @@ export async function signup(prevState, formData) {
   if (Object.keys(errors).length > 0) return { errors };
 
   //store it in the database (create a new user)
+  //password를 평문으로 저장하게 되면 보안상 문제가 생길 수 있음 (해시처리 필수)
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword); // 세션 생성
+    await createAuthSession(id);
+    redirect("/training");
+  } catch (error) {
+    //sqlite는 unique 조건 위반시 에러 발생
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "It seems like an account for the chosen email already exists.",
+        },
+      };
+    }
+    throw error;
+  }
 }
